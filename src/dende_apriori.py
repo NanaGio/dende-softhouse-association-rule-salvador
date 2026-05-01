1c067126fa1137f7db43407e7019a6ff0dc43811
NanaGio <giovanna000gio@outlook.com>
2026-05-01 17:20:54 -0300

    feature/primeira parte da logica APRIORI

--- src/dende_apriori.py
+++ src/dende_apriori.py
@@ -9,15 +9,15 @@ class Apriori:
     def calcular_suporte(self, item):
         return self.df[item].mean()
 
-    def obter_item_frequente(self, itemset):
+    def obter_item_frequente(self):
         itemset = {}
         treshold = 0.05
+
         for item in self.df.columns:
-            c1_unicos = item.value_counts()
             suporte_item = self.calcular_suporte(item)
             if suporte_item >= treshold:
                 itemset[item] = suporte_item
-            return itemset
+        return itemset
 
 
 
